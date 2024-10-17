876f3d61032b76d9deb4e09463e5ea1fb5527eb7
McDonnell, Marshall <mcdonnellmt@ornl.gov>
2024-10-17 12:31:26 -0400

    pycaspy ex1 - scans periodically

--- src/python_epics_tutorials/pcaspy_tutorials/ex1_client.py
+++ src/python_epics_tutorials/pcaspy_tutorials/ex1_client.py
@@ -1,13 +1,9 @@
-from epics import caget, caput, cainfo
+from epics import caget
+import time
 
-caput("MTEST:RAND", 0)
-cainfo("MTEST:RAND")
-
-caput("MTEST:RAND", -1.23)
-cainfo("MTEST:RAND")
-
-out = caget("MTEST:RAND")
-print("CAGET output:", out)
+while True:
+    time.sleep(0.5)
+    print(caget("MTEST:RAND"))
 
 
 
--- src/python_epics_tutorials/pcaspy_tutorials/ex1_server_random_number.py
+++ src/python_epics_tutorials/pcaspy_tutorials/ex1_server_random_number.py
@@ -1,16 +1,27 @@
 from pcaspy import Driver, SimpleServer
+import random
 
 prefix = "MTEST:"
 
 pvdb = {
     "RAND": {
         "prec": 3,
+        "scan": 1,
     }
 }
 
 class myDriver(Driver):
     def __init__(self):
         super(myDriver, self).__init__()
+
+    def read(self, reason):
+        print(reason)
+        if reason == "RAND":
+            value = random.random()
+        else:
+            value = self.getParam(reason)
+        print(value)
+        return value
 
 
 if __name__ == "__main__":
